e5b0f6c94a9fd0b348e0d4a1edccad2ebd7c7549
Adam Bartonicek <bartonicek@gmail.com>
2023-11-28 16:05:23 +0100

    Added fluctuation plot to build

--- lib/main.ts
+++ lib/main.ts
@@ -5,5 +5,6 @@ export { Scene } from "./dom/scene/Scene.ts";
 export { Dataframe } from "./structs/Dataframe";
 export { loadData } from "./utils/funs";
 export { BarPlot } from "./wrappers/BarPlot";
+export { FluctuationPlot } from "./wrappers/FluctuationPlot.ts";
 export { HistoPlot } from "./wrappers/HistoPlot";
 export { ScatterPlot } from "./wrappers/ScatterPlot";
